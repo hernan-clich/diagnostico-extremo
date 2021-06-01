75696757440e5494a0957bc694f64010b50d5667
unknown <cervece41@gmail.com>
2021-06-01 12:42:10 -0300

    Updated prices, June 2021

--- src/precios.js
+++ src/precios.js
@@ -1,12 +1,12 @@
 export const precios = {
-    Dominio: 1899,
-    Otros: "Consultar por otras localidades",
-    zonas: {
-        "CABA": 3999
-    }, 
-    links: {
-        Escaneo: "https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=448382769-ca971c73-303c-4b32-9100-4f8cf2cc8248",
-        Dominio: "https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=448382769-5ae24d30-82b9-4bf6-8169-21d7a97b116b",
-        informeFull: "https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=448382769-efe40457-bd58-4866-a378-1510e47c49c8"
-    }
+  Dominio: 2499,
+  Otros: "Consultar por otras localidades",
+  zonas: {
+    "CABA": 4499
+  }, 
+  links: {
+    Escaneo: "https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=448382769-ca971c73-303c-4b32-9100-4f8cf2cc8248",
+    Dominio: "https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=448382769-5ae24d30-82b9-4bf6-8169-21d7a97b116b",
+    informeFull: "https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=448382769-efe40457-bd58-4866-a378-1510e47c49c8"
+  }
 }
