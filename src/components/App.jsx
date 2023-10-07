943f428dc32ff0d650796f7f5dc89393ed386afd
hernan-clich <hernan.g.clich@gmail.com>
2023-10-07 19:50:45 -0300

    Wrapping app in query provider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useRef } from 'react';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
-import {GlobalStyles} from '../styles/GlobalStyles'
+
+import { GlobalStyles } from '../styles/GlobalStyles'
 import Header from './Header';
 import Hero from './Hero';
 import About from './About';
@@ -19,34 +24,36 @@ function App() {
   const testimonialsRef = useRef(null);
   const heroRef = useRef(null);
 
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles overflowY={bodyOverflowY} />
       <Header
         setBodyOverflowY={setBodyOverflowY}
         ref={{
           heroRef,
           servicesRef,
-          stepsRef, 
-          pricingRef, 
-          testimonialsRef
+          stepsRef,
+          pricingRef,
+          testimonialsRef,
         }}
       />
-      <Hero ref={heroRef}/>
-      <About/>
-      <Steps ref={stepsRef}/>
-      <Services ref={servicesRef}/>
-      <Pricing ref={pricingRef}/>
-      <Testimonials ref={testimonialsRef}/>
-      <Contact/>
+      <Hero ref={heroRef} />
+      <About />
+      <Steps ref={stepsRef} />
+      <Services ref={servicesRef} />
+      <Pricing ref={pricingRef} />
+      <Testimonials ref={testimonialsRef} />
+      <Contact />
       <Footer
         ref={{
           servicesRef,
-          stepsRef, 
-          pricingRef
+          stepsRef,
+          pricingRef,
         }}
       />
-    </>
+    </QueryClientProvider>
   );
 }
 
