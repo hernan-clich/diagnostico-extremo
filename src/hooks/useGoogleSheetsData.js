d5da88004ada1e104cd598851b4ecff8bafdd876
hernan-clich <hernan.g.clich@gmail.com>
2023-10-07 19:50:22 -0300

    Created custom hook to fetch csv data

---
+++ src/hooks/useGoogleSheetsData.js
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { parseCSV } from '../utils/parseCsv'
+
+export const useGoogleSheetsCsvData = (url) => {
+  const { data } = useQuery({
+    queryKey: ["diagex-prices"],
+    queryFn: () =>
+      fetch(url)
+        .then((res) => {
+          if (res.ok) {
+            return res.text();
+          }
+        })
+        .then((csvData) => parseCSV(csvData)),
+  }, {
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false
+  });
+
+
+  return data;
+}
